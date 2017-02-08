947a0c2b81afe91549b6554b5b42e302e5f7671e
Felix Wetell <felix.wetell@gmail.com>
2017-02-08 12:41:28 +0100

    added express-ejs-layouts

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 
 var app = express();
 var urlParser = bodyParser.urlencoded();
+
 app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/' + 'index.html')
+
+    var locals = {
+    title: 'Page Title',
+    description: 'Page Description',
+    header: 'Page Header'
+  };
+  res.render('the-view', locals);
 });
 
+ 
 var server = app.listen(8088, function(){
     var host = server.address().address;
     var port = server.address().address;
