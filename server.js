d3e8e1056dee41fddec57c99c051f8a14bb4d45d
Felix Wetell <felix.wetell@gmail.com>
2017-02-08 11:41:53 +0100

    moved around files and webpage now working

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var app = express();
 var urlParser = bodyParser.urlencoded();
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/' + 'index.html')
 });
 
+var server = app.listen(8088, function(){
+    var host = server.address().address;
+    var port = server.address().address;
+
+    console.log('express app listening to ' + host + " " + port)
+});
